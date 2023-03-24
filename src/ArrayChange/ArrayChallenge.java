1582a0086f56683f82c4caec1e8d23066ebebe41
Ihame-b <ihamegrbt@gmail.com>
2023-03-24 21:52:29 +0200

    adding Bank App

--- src/ArrayChange/ArrayChallenge.java
+++ src/ArrayChange/ArrayChallenge.java
@@ -1,3 +1,5 @@
+package ArrayChange;
+
 import java.util.ArrayList;
 import java.util.List;
 
--- src/ArrayChange/Main.java
+++ src/ArrayChange/Main.java
@@ -1,3 +1,7 @@
+package ArrayChange;
+
+import ArrayChange.ArrayChallenge;
+
 import java.util.Arrays;
 
 public class Main {
